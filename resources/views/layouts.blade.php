01d6c2e568c89a983d4c92c4d9b58a4440197793
useryesa9@gmail.com <useryesa9@gmail.com>
2024-07-23 15:10:48 +0700

    menambah icon di title

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link rel="icon" href="{{ asset('assets/images/graha.png') }}" type="image/x-icon">
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -80,17 +81,14 @@
 
     </header>
 
-    <main class="main">
+<main class="main">
         <br>
         <br>
-
         @yield('content')
 
     </main>
 
     <footer id="footer" class="footer accent-background">
-
-
 
         <div class="container copyright text-center mt-4">
             <p>© <span>Copyright</span> <strong class="px-1 sitename">Yesa</strong> <span>All Rights Reserved</span>
