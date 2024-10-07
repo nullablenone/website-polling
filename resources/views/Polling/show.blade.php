736e5c63668a6ff8893246252941a136f85a6b72
useryesa9@gmail.com <useryesa9@gmail.com>
2024-10-07 09:27:28 +0700

    membuat fitur share

--- resources/views/Polling/show.blade.php
+++ resources/views/Polling/show.blade.php
@@ -38,25 +38,77 @@
                     <a class="btn btn-success mx-2 shadow" href="{{ route('polling.pollingTerbaru') }}">
                         <i class="fa fa-check-circle mr-2 text-light" aria-hidden="true"></i> Polling Tersimpan
                     </a>
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class="fa mr-2 fa-share-alt"></i>
+                        Bagikan
+                    </button>
+
+
+                    {{-- --------------------------- fungsi share --------------------- --}}
+                    <ul class="dropdown-menu">
+
+                        <!-- WhatsApp -->
+                        <li>
+                            <a id="whatsappShare" class="dropdown-item d-flex align-items-center" href="#"
+                                style="text-decoration: none;">
+                                <i class="fab fa-whatsapp" style="font-size: 18px; color: #25D366; margin-right: 8px;"></i>
+                                <span>Share via WhatsApp</span>
+                            </a>
+                        </li>
+
+                        <!-- Telegram -->
+                        <li>
+                            <a id="telegramShare" class="dropdown-item d-flex align-items-center" href="#"
+                                style="text-decoration: none;">
+                                <i class="fab fa-telegram-plane"
+                                    style="font-size: 18px; color: #0088cc; margin-right: 8px;"></i>
+                                <span>Share via Telegram</span>
+                            </a>
+                        </li>
+
+                        <!-- Facebook -->
+                        <li>
+                            <a id="facebookShare" class="dropdown-item d-flex align-items-center" href="#"
+                                style="text-decoration: none;">
+                                <i class="fab fa-facebook-f"
+                                    style="font-size: 18px; color: #1877F2; margin-right: 8px;"></i>
+                                <span>Share via Facebook</span>
+                            </a>
+                        </li>
+
+                        <!-- Twitter -->
+                        <li>
+                            <a id="twitterShare" class="dropdown-item d-flex align-items-center" href="#"
+                                style="text-decoration: none;">
+                                <i class="fab fa-twitter" style="font-size: 18px; color: #1DA1F2; margin-right: 8px;"></i>
+                                <span>Share via X (Twitter)</span>
+                            </a>
+                        </li>
+                    </ul>
+
+
+
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="alert alert-info text-center mt-4 card shadow-lg">
-            <h5 class="alert-heading"><strong>TENTANG POLLING INI</strong></h5>
-            <p>Polling tentang <span class="fw-bold"><a href="#"
-                        class="text-info text-decoration-none">{{ $polling->title }}</a></span> dibuat pada <span
-                    class="fw-bold">{{ $polling->created_at->format('d-m-Y') }}</span></p>
-            <p>Polling ini memiliki opsi jawaban dan sudah menerima <span class="fw-bold"
-                    id="cVote">{{ $polling->jawaban->sum('vote') }}</span> suara.</p>
-            <p class="mb-0">Melakukan pemilihan berulang kali tidak diperbolehkan. Pemeriksaan duplikasi didasarkan pada
-                alamat IP pemilih. Kami tidak mentolerir setiap kecurangan yang dilakukan dan akan menganulir semua suara
-                yang berindikasi dilakukan oleh bot.</p>
-        </div>
+    <div class="alert alert-info text-center mt-4 card shadow-lg">
+        <h5 class="alert-heading"><strong>TENTANG POLLING INI</strong></h5>
+        <p>Polling tentang <span class="fw-bold"><a href="#"
+                    class="text-info text-decoration-none">{{ $polling->title }}</a></span> dibuat pada <span
+                class="fw-bold">{{ $polling->created_at->format('d-m-Y') }}</span></p>
+        <p>Polling ini memiliki opsi jawaban dan sudah menerima <span class="fw-bold"
+                id="cVote">{{ $polling->jawaban->sum('vote') }}</span> suara.</p>
+        <p class="mb-0">Melakukan pemilihan berulang kali tidak diperbolehkan. Pemeriksaan duplikasi didasarkan pada
+            alamat IP pemilih. Kami tidak mentolerir setiap kecurangan yang dilakukan dan akan menganulir semua suara
+            yang berindikasi dilakukan oleh bot.</p>
+    </div>
 
-        <div class="text-center">
-            <a href="{{ route('polling.create') }}" class="btn btn-danger mt-4 shadow">Buat Polling Anda Sendiri</a>
-        </div>
+    <div class="text-center">
+        <a href="{{ route('polling.create') }}" class="btn btn-danger mt-4 shadow">Buat Polling Anda Sendiri</a>
+    </div>
     </div>
 
     <!-- JavaScript -->
@@ -66,5 +118,52 @@
                 document.getElementById('formVote').submit();
             });
         });
+
+
+
+
+        // ------------------------ fungsi share -----------------------------
+
+
+        const text = `
+        Ayo ikuti Polling ini!
+
+        {{ $polling->title }}
+
+        Hasil sementara:
+        @foreach ($polling->jawaban as $jawaban)
+        - {{ $jawaban->option }}: {{ $jawaban->vote }}%
+        @endforeach
+
+        Total suara yang sudah masuk: {{ $polling->jawaban->sum('vote') }}
+
+        Jangan sampai ketinggalan, berikan suaramu sekarang!
+        Klik link berikut untuk ikut serta: http://website-polling.test/polling/{{ $polling->id }}
+        `.trim();
+
+
+
+        // ------------------ fungsi share WhatsApp
+        const whatsappShare = document.getElementById('whatsappShare');
+        whatsappShare.href =
+            `https://api.whatsapp.com/send?text=${encodeURIComponent(text)}%0Ahttp://website-polling.test:8080/{{ $polling->id }}`;
+
+
+        // ------------------ fungsi share telegram
+        const telegramShare = document.getElementById('telegramShare');
+        telegramShare.href =
+            `https://t.me/share/url?url=${encodeURIComponent('http://website-polling.test:8080/{{ $polling->id }}')}&text=${encodeURIComponent(text)}`;
+
+
+        // ------------------ fungsi share facebookShare
+        const facebookShare = document.getElementById('facebookShare');
+        facebookShare.href =
+            `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent('http://website-polling.test:8080/{{ $polling->id }}')}&quote=${encodeURIComponent(text)}`;
+
+
+        // ------------------ fungsi share twiter
+        const twitterShare = document.getElementById('twitterShare');
+        twitterShare.href =
+            `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent('http://website-polling.test:8080/{{ $polling->id }}')}`;
     </script>
 @endsection
