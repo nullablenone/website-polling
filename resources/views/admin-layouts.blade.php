022408d060fbd1c4b2aa978532c50f345bec14c1
useryesa9@gmail.com <useryesa9@gmail.com>
2024-10-27 13:10:56 +0700

    memperbarui

--- resources/views/admin-layouts.blade.php
+++ resources/views/admin-layouts.blade.php
@@ -110,30 +110,30 @@
 
                 <ul class="menu-inner py-1">
                     <!-- Dashboard -->
-                    <li class="menu-item active">
-                        <a href="index.html" class="menu-link">
+                    <li class="menu-item {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+                        <a href="{{ route('admin.dashboard') }}" class="menu-link">
                             <i class="menu-icon tf-icons bx bx-home-circle"></i>
                             <div data-i18n="Analytics">Dashboard</div>
                         </a>
                     </li>
 
-                    <!-- Layouts -->
-                    <li class="menu-item">
+                    <!-- User Layouts -->
+                    <li class="menu-item {{ request()->routeIs('admin.statistik') ? 'active open' : '' }}">
                         <a href="javascript:void(0);" class="menu-link menu-toggle">
                             <i class="menu-icon tf-icons bx bx-layout"></i>
                             <div data-i18n="Layouts">User</div>
                         </a>
-
-                        <ul class="menu-sub">
-                            <li class="menu-item">
+                        <ul class="menu-sub {{ request()->routeIs('admin.statistik') ? 'show' : '' }}">
+                            <li class="menu-item {{ request()->routeIs('admin.statistik') ? 'active' : '' }}">
                                 <a href="{{ route('admin.statistik') }}" class="menu-link">
                                     <div data-i18n="Without menu">Statistik</div>
                                 </a>
                             </li>
                         </ul>
                     </li>
-
                 </ul>
+
+
             </aside>
             <!-- / Menu -->
 
