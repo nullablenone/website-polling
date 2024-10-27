ac5d7c9e4e28e7fdde4cfbfcfb10b04f04a5ccd9
useryesa9@gmail.com <useryesa9@gmail.com>
2024-10-27 12:08:43 +0700

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -16,10 +17,31 @@ class UserSeeder extends Seeder
         $user = User::create([
             'name' => 'admin@gmail.com',
             'email' => 'admin@gmail.com',
-            'password' => bcrypt('password'),
+            'password' => Hash::make('admin@gmail.com'),
             'ip_address' => request()->ip(),
         ]);
 
         $user->assignRole('admin');
+
+        User::create([
+            'name' => 'yesa@gmail.com',
+            'email' => 'yesa@gmail.com',
+            'password' => Hash::make('yesa@gmail.com'),
+            'ip_address' => '127.4.5.4',
+        ]);
+
+        User::create([
+            'name' => 'ranu@gmail.com',
+            'email' => 'ranu@gmail.com',
+            'password' => Hash::make('ranu@gmail.com'),
+            'ip_address' => '127.4.4.4',
+        ]);
+
+        User::create([
+            'name' => 'perdi@gmail.com',
+            'email' => 'perdi@gmail.com',
+            'password' => Hash::make('perdi@gmail.com'),
+            'ip_address' => '127.0.3.2',
+        ]);
     }
 }
