9da611ec4eaa2a8b3ffb714d03b8bf0a162f2aa2
useryesa9@gmail.com <useryesa9@gmail.com>
2024-10-28 16:27:36 +0700

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -14,34 +14,48 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
+        // Buat admin user
+        $admin = User::create([
             'name' => 'admin@gmail.com',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin@gmail.com'),
             'ip_address' => request()->ip(),
         ]);
+        $admin->assignRole('admin');
 
-        $user->assignRole('admin');
+        // List of regular users
+        $users = [
+            [
+                'name' => 'yesa@gmail.com',
+                'email' => 'yesa@gmail.com',
+                'password' => Hash::make('yesa@gmail.com'),
+                'ip_address' => '127.4.5.4',
+            ],
+            [
+                'name' => 'ranu@gmail.com',
+                'email' => 'ranu@gmail.com',
+                'password' => Hash::make('ranu@gmail.com'),
+                'ip_address' => '127.4.4.4',
+            ],
+            [
+                'name' => 'perdi@gmail.com',
+                'email' => 'perdi@gmail.com',
+                'password' => Hash::make('perdi@gmail.com'),
+                'ip_address' => '127.0.3.2',
+            ],
+        ];
 
-        User::create([
-            'name' => 'yesa@gmail.com',
-            'email' => 'yesa@gmail.com',
-            'password' => Hash::make('yesa@gmail.com'),
-            'ip_address' => '127.4.5.4',
-        ]);
+        // Loop untuk buat user biasa dan insert batas_polling
+        foreach ($users as $userData) {
+            $user = User::create($userData);
 
-        User::create([
-            'name' => 'ranu@gmail.com',
-            'email' => 'ranu@gmail.com',
-            'password' => Hash::make('ranu@gmail.com'),
-            'ip_address' => '127.4.4.4',
-        ]);
-
-        User::create([
-            'name' => 'perdi@gmail.com',
-            'email' => 'perdi@gmail.com',
-            'password' => Hash::make('perdi@gmail.com'),
-            'ip_address' => '127.0.3.2',
-        ]);
+            // Insert batas_polling untuk user yang bukan admin
+            DB::table('batas_pollings')->insert([
+                'user_id' => $user->id,
+                'ip_address' => $user->ip_address,
+                'jumlah_polling' => 0,
+                'batas_polling' => 3,
+            ]);
+        }
     }
 }
