67a2cfb03dbdbc203eabf9050857986b29d217a5
useryesa9@gmail.com <useryesa9@gmail.com>
2026-06-29 16:01:42 +0700

    refactor: migrasi CSS, JS template, dan vendor ke Laravel Vite
    - Install pustaka vendor via npm (bootstrap, swiper, aos, dll)
    - Gabungkan custom CSS & JS ke folder resources untuk dibundel
    - Ganti pemanggilan aset manual di layouts dengan direktif @vite
    - Hapus folder css, js, vendor, dan scss yang tidak digunakan di public/assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+// Import Vendors from node_modules
+import AOS from 'aos';
+import GLightbox from 'glightbox';
+import Swiper from 'swiper/bundle'; // Swiper bundle includes all modules
+import PureCounter from '@srexi/purecounterjs';
+import imagesLoaded from 'imagesloaded';
+import Isotope from 'isotope-layout';
+import 'bootstrap'; // Import Bootstrap Javascript
+import '../vendor/php-email-form/validate.js'; // Keep local validation script
+
+// Bind to window so main.js can access them globally
+window.AOS = AOS;
+window.GLightbox = GLightbox;
+window.Swiper = Swiper;
+window.PureCounter = PureCounter;
+window.imagesLoaded = imagesLoaded;
+window.Isotope = Isotope;
+
+// Import main template JS
+import './main.js';
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -25,17 +25,9 @@
     {{-- cdn fontawesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <!-- Load Compiled & Minified Assets with Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- Main CSS File -->
-    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
-    <!-- Custom Luxury CSS -->
-    <link href="{{ asset('assets/css/luxury.css') }}" rel="stylesheet">
 
 
 </head>
@@ -136,19 +128,6 @@
 
     <!-- Preloader -->
     <div id="preloader"></div>
-
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-
-    <!-- Main JS File -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
 
 
 
